559e40711293754ddccaef2e514a82877a10f2bb
Jean Bustos <dev.bjean@gmail.com>
2022-10-12 02:27:40 -0300

    cuarto avance

--- TIM4/TIM4.cpp
+++ TIM4/TIM4.cpp
@@ -5,25 +5,25 @@ using namespace std;
 
 class Proveedor{
 	private://atributos
-		string a;
+		string search;
 	public://metodos
 		Proveedor(string);//constructor
-		string validarPedido();//metodo 1
-		string validarProducto();//metodo 2
-		string validarFecha();//metodo 3
+		int validarPedido();//metodo 1
+		int validarProducto();//metodo 2
+		int validarFecha();//metodo 3
 };
 
-Proveedor::Proveedor(string _a){
-	a = _a;	
+Proveedor::Proveedor(string _search){
+	search = _search;	
 }
 
 //metodo 1
-string Proveedor::validarPedido(){
+int Proveedor::validarPedido(){
 	string line;
 	ifstream indata("proveedores.txt");
 	while (getline(indata, line)) {
 
-	    if (a == line.substr(0,10)) {
+	    if (search == line.substr(0,10)) {
 	    	cout << "El registro fue encontrado, los datos son los siguientes:\n" << endl;
 	      
 	    	cout << "Pedido nro		: " << line.substr(0,10) << endl;
@@ -39,13 +39,13 @@ string Proveedor::validarPedido(){
 	return 0;
 }
 //metodo 2
-string Proveedor::validarProducto(){
+int Proveedor::validarProducto(){
 	string line;
 	ifstream indata("proveedores.txt");
 	while (getline(indata, line)) {
 
-	    if (a == line.substr(10,10)) {
-	    	cout << "El registro fue encontrado, los datos son los siguientes:\n" << endl;
+	    if (search == line.substr(10,10)) {
+			cout << "El registro fue encontrado, los datos son los siguientes:\n" << endl;
 	      
 			cout << "Codigo producto		: " << line.substr(10,10) << endl; 
 	    	cout << "Pedido nro		: " << line.substr(0,10) << endl;
@@ -60,12 +60,12 @@ string Proveedor::validarProducto(){
 	return 0;
 }
 //metodo 3
-string Proveedor::validarFecha(){
+int Proveedor::validarFecha(){
 	string line;
 	ifstream indata("proveedores.txt");
 	while (getline(indata, line)) {
 
-	    if (a == line.substr(52,8)) {
+	    if (search == line.substr(52,8)) {
 	    	cout << "El registro fue encontrado, los datos son los siguientes:\n" << endl;
 	      
 			cout << "Fecha de entrega	: " << line.substr(52,2) <<"/" << line.substr(54,2) << "/" << line.substr(56,4) << endl;
@@ -82,7 +82,6 @@ string Proveedor::validarFecha(){
 }
 //menu
 int menu(){
-	system("cls");
 	int opcion;
 	cout << "<<			Trabajo Individual Modulo 4			>>" << endl << endl;
 	cout << "1.- Buscar un pedido" << endl;
@@ -90,12 +89,13 @@ int menu(){
 	cout << "3.- Buscar por fecha" << endl;
 	cout << "4.- Salir" << endl << endl;
 	cout << "Digite una opcion: "; cin >> opcion;
+	cout << endl;
 	return opcion;
 }
 
 int main(){
-	ifstream indata("proveedores.txt");
-  	string line;
+	//ifstream indata("proveedores.txt");
+  	//string line;
   	string search;
   	string a, b, c;
   	int opcion;
@@ -111,83 +111,20 @@ int main(){
 	  	cout << "Buscar el codigo: ";
   		cin >> search;
   		cout << endl;
-  		Proveedor prov2 = Proveedor(search);
-  		b = prov2.validarProducto();
+  		Proveedor prov1 = Proveedor(search);
+  		b = prov1.validarProducto();
   		cout << b << endl;
 	  }else if(opcion == 3){
 	  	cout << "Buscar por la fecha: ";
   		cin >> search;
   		cout << endl;
-  		Proveedor prov3 = Proveedor(search);
-  		c = prov3.validarFecha();
+  		Proveedor prov1 = Proveedor(search);
+  		c = prov1.validarFecha();
   		cout << c << endl;
 	  }else{
 	  	return 0;
 	  }
   	
-  	
-  	/*int op;
-  	
-  	do{
-	  	system("cls");
-	  	op = menu();
-	  	switch(op){
-	  			
-		  	case 1:
-	  			cout << "Buscar un pedido\n";
-  				cin >> search;
-  				Proveedor prov1 = Proveedor(search);
-  				a = prov1.validarPedido();
-  				cout << a << endl;	
-  			break;
-  			case 2:
-  				//cout << "Buscar un codigo\n";
-  				//cin >> search;
-  				//Proveedor prov2 = Proveedor(search);
-  				//b = prov2.validarProducto();
-  				//cout << b << endl;
-  			break;
-  			case 3:
-  				cout << "Buscar una fecha\n";
-  				cin >> search;
-  				//Proveedor prov3 = Proveedor(search);
-  				//c = prov3.validarFecha();
-  				//cout << c << endl;
-  			break;
-  			case 4:
-  				return 0;
-  			break;
-		}
-  	}while(op != 3);
-  	return 0;*/
 
-  	/*cout << "Buscar un pedido\n";
-  	cin >> search;
-  
-  	Proveedor prov1 = Proveedor(search);
-  	a = prov1.validarPedido();
-  	b = prov1.validarProducto();
-  	c = prov1.validarFecha();
-  
-  	cout << a << endl;
-  	cout << b << endl;
-  	cout << c << endl;*/
-
-	/*while (getline(indata, line)) {
-    	//istringstream aux(line);	
-    	string a, b;
-
-    //if (!(aux)) { break; }
-	
-    if (search == line.substr(0,10)) {
-      	cout << "El registro fue encontrado!\n";
-      	cout << a << endl << b << endl;
-    	cout << line;
-    	return 0;
-    	}
-  	}
-
-  	cout << "El registro no fue encontrado\n";*/
-
-  	//return 0;
+  	return 0;
 }
